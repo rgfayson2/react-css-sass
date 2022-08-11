b0a1397b15003178f4bb638ebf6a6f19a78431b8
Roderick Fayson <rgfayson2@gmail.com>
2022-08-11 16:15:35 -0400

    sass-css

---
+++ src/components.js/Header.js
@@ -0,0 +1,12 @@
+ const Header = () => {
+    return (
+        <ul className="navigation">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
+
+export default Header
